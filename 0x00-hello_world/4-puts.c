ed522f6125d31cdebc80aa8ef913f0cbf58a38f3
KEMJIKAB <kemjikab26@gmail.com>
2023-02-16 16:09:13 -0800

    commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main (void)
+{
+    puts("programming is like building a multilingua puzzle \n");
+    return (0);
+}
